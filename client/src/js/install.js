72033c9242399538030a11b8b7e5b4442d891c94
Gabe Morris <gabemorris1042@gmail.com>
2023-03-19 11:13:01 -0300

    added logic to the butInstall listeners

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,32 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // grab the event 
+  window.defferedPrompt = event;
+
+  // make the install button visible to the user to install
+  butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  // checks to see if defferedPrompt has been set in beforeInstallPrompt function
+  const promptUser = window.defferedPrompt;
+  if (!promptUser) return;
+
+  // otherwise prompt the user
+  promptUser.prompt();
+
+  // after prompt reset defferedPrompt back to null
+  window.defferedPrompt = null;
+
+  // now that app is installed hide the button
+  butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  // checks if app is already installed, if so resets defferedPrompt to null
+  window.defferedPrompt = null;
+});
